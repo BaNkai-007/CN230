8ed1a41c6579a18a383e236143492a27c1848085
SasmitDeySarkar <152879496+SasmitDey@users.noreply.github.com>
2024-03-23 15:58:20 +0530

    Delete KnifeDrop directory

--- KnifeDrop/recipe/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-from django.contrib.auth.models import User
-# Create your models here.
-
-class Recipe(models.Model):
-    title = models.CharField(max_length=100)
-    description = models.TextField()
-
-    author = models
